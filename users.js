fe0ff29bb5858263b3e88355e9f5d85b467a8c46
salman k <salman371k@gmail.com>
2024-04-19 23:12:43 +0530

    add put and delete in users

--- users.js
+++ users.js
@@ -11,15 +11,11 @@ let users = []
 app.post('/user', (req,res)=>{
   users.push({ id:req.body.id ,name:req.body.name , email:req.body.email , password :req.body.password})
   console.log(users)
-  res.send('ok')
+  res.send(users)
 })
 
 app.get('/user' , (req,res)=>{
     res.send(users)
-})
-
-app.put('/user',(req,res)=>{
-    req
 })
 
 app.get('/user/:id', (req,res)=>{
@@ -28,6 +24,29 @@ app.get('/user/:id', (req,res)=>{
     const data = users.filter((value)=> value.id==userid)
     console.log(data);
     res.send(data)
+})
+
+app.put('/user/:id', (req, res)=>{
+    const userId = req.params.id
+    console.log(userId);
+    const newdata = {id:userId,name:req.body.name , email:req.body.email , password :req.body.password}
+      let index = 0
+     users.map((value , i)=>{
+       if( value.id==userId){
+          index+= i
+       }
+    } )
+     users[index] = newdata
+    res.send(users)
+})
+
+app.delete('/user/:id',(req,res)=>{
+    const userId = req.params.id
+    const newdata = users.filter((value)=> value.id !=userId)
+    console.log(newdata);
+    users = newdata
+    
+     res.send(users)
 })
 
 
